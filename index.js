b5c54c97f925bc0bb284f7e297f3fbf52ff41eb5
Denis Tsoi <denistsoi@gmail.com>
2015-12-18 01:06:10 +0800

    ADD: [project] tidy the project structure; add imagination.html;

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var express = require('express');
 var port = process.env.PORT || 8000;
 
 express()
-  .use(quesadilla(__dirname + '/scss'))
+  .use(quesadilla(__dirname + '/_scss'))
   .use(express.static(__dirname))
   .listen(port, function() {
     console.log('Server is listening on port', port);
--- push_index.js
+++ push_index.js
@@ -6,8 +6,7 @@ var sass = require('node-sass');
 var fs = require('fs');
 
 sass.render({
-    file: 'scss/main.scss',
-    outFile: 'scss/main.scss',
+    file: '_scss/main.scss',
   }, function(error, result) { // node-style callback from v3.0.0 onwards
     if (error) console.log(error);
     if(!error){
